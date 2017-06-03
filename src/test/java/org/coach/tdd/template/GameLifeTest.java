7b5434f545bafafcf389fd74db7dcd591c2eaa13
XvWenJun <906626481@qq.com>
2017-06-03 12:23:43 +0800

    clean project

--- src/test/java/org/coach/tdd/template/GameLifeTest.java
+++ src/test/java/org/coach/tdd/template/GameLifeTest.java
@@ -9,20 +9,5 @@ import org.junit.Test;
 
 public class GameLifeTest {
 
-    private GameLife classUnderTest;
-
-    @Before
-    public void setUp() throws Exception {
-        int status[][] = {{1, 1, 1},{1, 0, 1},{1, 1, 1}};
-        classUnderTest = new GameLife(status);
-
-    }
-
-
-    @Test
-    public void shouldAliveOnlyThreeAliveAround() throws Exception {
-        int act = classUnderTest.checkStatus(1, 1, 1);
-        int exp = 1;
-        assertEquals(act, exp);
-    }
+   
 }
