67322c864541518fbedcbe5f835d9bd069239b9a
XvWenJun <906626481@qq.com>
2017-06-03 13:58:27 +0800

    rafactor/ rename method

--- src/main/java/org/coach/tdd/template/GameLife.java
+++ src/main/java/org/coach/tdd/template/GameLife.java
@@ -12,40 +12,23 @@ public class GameLife {
         kWidth = cellMap[0].length;
     }
 
-    private void printCellMap(int[][] map) {
+    public int[][] updateCellMap() {
+        calculateNextCellMap();
+        return cellMap;
+    }
+
+    public void calculateNextCellMap() {
+        int[][] nextCellMap = new int [kHeight][kWidth];
         for (int i = 0; i < kHeight ; i++) {
             for (int j = 0; j < kWidth ; j++) {
-                System.out.println(cellMap[i][j]);
+                nextCellMap[i][j] = updateCellState(i, j);
             }
         }
+        cellMap = nextCellMap;
     }
 
-
-    public int getAliveNumber(int x, int y) {
-        int sum = 0;
-        for (int i = x - 1; i <= x + 1; i++) {
-            for (int j = y - 1; j <= y + 1 ; j++) {
-                if (i == x && j == y) {
-                    continue;
-                } else {
-                    sum += isCellAlive(i, j);
-                }
-            }
-        }
-
-        return sum;
-    }
-
-    private int isCellAlive(int x, int y) {
-        if (x >= 0 && x < kWidth && y >= 0 && y < kHeight) {
-            return cellMap[x][y];
-        }
-
-        return 0;
-    }
-
-    public int getCellState(int x, int y) {
-        int cellAroundAliveNumber = getAliveNumber(x, y);
+    public int updateCellState(int x, int y) {
+        int cellAroundAliveNumber = calculateCellAroundAliveNumber(x, y);
 
         int result;
         if (cellAroundAliveNumber == 3) {
@@ -59,18 +42,26 @@ public class GameLife {
         return result;
     }
 
-    public int[][] getCellMap() {
-        nextCellMap();
-        return cellMap;
-    }
-
-    public void nextCellMap() {
-        int[][] nextArr = new int [kHeight][kWidth];
-        for (int i = 0; i < kHeight ; i++) {
-            for (int j = 0; j < kWidth ; j++) {
-                nextArr[i][j] = getCellState(i, j);
+    public int calculateCellAroundAliveNumber(int x, int y) {
+        int sum = 0;
+        for (int i = x - 1; i <= x + 1; i++) {
+            for (int j = y - 1; j <= y + 1 ; j++) {
+                if (i == x && j == y) {
+                    continue;
+                } else {
+                    sum += getCellState(i, j);
+                }
             }
         }
-        cellMap = nextArr;
+
+        return sum;
+    }
+
+    private int getCellState(int x, int y) {
+        if (x >= 0 && x < kWidth && y >= 0 && y < kHeight) {
+            return cellMap[x][y];
+        }
+
+        return 0;
     }
 }
--- src/test/java/org/coach/tdd/template/GameLifeTest.java
+++ src/test/java/org/coach/tdd/template/GameLifeTest.java
@@ -11,27 +11,27 @@ public class GameLifeTest {
     public void shouldReturnTheNumberOfAliveAround() throws Exception {
         int[][] arr = {{1, 1, 1}, {1, 0, 1}, {1, 0, 0}};
         GameLife gameLife = new GameLife(arr);
-        assertEquals(2, gameLife.getAliveNumber(0, 0));
-        assertEquals(4, gameLife.getAliveNumber(0, 1));
-        assertEquals(2, gameLife.getAliveNumber(0, 2));
-        assertEquals(3, gameLife.getAliveNumber(1, 0));
+        assertEquals(2, gameLife.calculateCellAroundAliveNumber(0, 0));
+        assertEquals(4, gameLife.calculateCellAroundAliveNumber(0, 1));
+        assertEquals(2, gameLife.calculateCellAroundAliveNumber(0, 2));
+        assertEquals(3, gameLife.calculateCellAroundAliveNumber(1, 0));
     }
 
     @Test
     public void shouldReturnCurrentCellState() throws Exception {
         int[][] arr = {{1, 1, 1}, {1, 0, 1}, {1, 0, 0}};
         GameLife gameLife = new GameLife(arr);
-        assertEquals(1, gameLife.getCellState(0, 0));
-        assertEquals(0, gameLife.getCellState(0, 1));
-        assertEquals(1, gameLife.getCellState(0, 2));
-        assertEquals(1, gameLife.getCellState(1, 0));
+        assertEquals(1, gameLife.updateCellState(0, 0));
+        assertEquals(0, gameLife.updateCellState(0, 1));
+        assertEquals(1, gameLife.updateCellState(0, 2));
+        assertEquals(1, gameLife.updateCellState(1, 0));
     }
 
     @Test
     public void shouldReturnLastCellMapState() throws Exception {
         int[][] arr = {{1, 1, 1}, {1, 0, 1}, {1, 0, 0}};
         GameLife gameLife = new GameLife(arr);
-        int[][] lastArr = gameLife.getCellMap();
+        int[][] lastArr = gameLife.updateCellMap();
         int[][] exp = {{1, 0, 1}, {1, 0, 1}, {0, 1, 0}};
         assertArrayEquals(exp, lastArr);
     }
