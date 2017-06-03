7331d53a528dc7d5d6deca0b03c4c3aed55c3d3f
XvWenJun <906626481@qq.com>
2017-06-03 16:43:50 +0800

    add function about setting speed

--- src/main/java/org/coach/tdd/template/GameLifeRunner.java
+++ src/main/java/org/coach/tdd/template/GameLifeRunner.java
@@ -1,9 +1,11 @@
 package org.coach.tdd.template;
 
-import java.awt.GridLayout;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import javax.swing.event.MenuKeyEvent;
+import javax.swing.event.MenuKeyListener;
 
 
 /**
@@ -14,8 +16,11 @@ public class GameLifeRunner {
     public static final int ROW_CELL = 20;
     public static final int COL_CELL = 20;
     public static final int ROW_CELL_WIDTH = 40;
-    public static final int DEPTH = 50;
-    public static final int SPEED = 100;
+    public static final int DEPTH = 500;
+    public static final String SLOW = "slow";
+    public static final String NORMAL = "normal";
+    public static final String FAST = "fast";
+    public static int SPEED = 100;
     private static JLabel[][] labels = new JLabel[ROW_CELL][COL_CELL];
     private static JFrame frame;
 
@@ -31,7 +36,7 @@ public class GameLifeRunner {
         }
         GameLife gameLife = new GameLife(initCellMap);
 
-        createAndShowGUI();
+        createFrameAndShow();
         for (int i = 0; i < DEPTH; i++) {
             int[][] cellMap = gameLife.updateCellMap();
             configLabels(cellMap);
@@ -39,7 +44,7 @@ public class GameLifeRunner {
     }
 
 
-    private static void createAndShowGUI() {
+    private static void createFrameAndShow() {
         JFrame.setDefaultLookAndFeelDecorated(true);
         frame = new JFrame("Game Life");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -51,6 +56,7 @@ public class GameLifeRunner {
         for (int i = 0; i < ROW_CELL; i++) {
             for (int j = 0; j < COL_CELL; j++) {
                 JLabel label = new JLabel();
+                label.setOpaque(true);
                 label.setText(String.valueOf(i));
                 frame.getContentPane().add(label);
                 labels[i][j] = label;
@@ -59,6 +65,45 @@ public class GameLifeRunner {
 
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+
+        createMenu();
+    }
+
+    private static void createMenu() {
+        JMenuBar menuBar = new JMenuBar();
+        frame.setJMenuBar(menuBar);
+
+        JMenu speedMenu = new JMenu("speed");
+
+        JMenuItem slowItem = new JMenuItem(SLOW);
+        JMenuItem normalItem = new JMenuItem(NORMAL);
+        JMenuItem fastItem = new JMenuItem(FAST);
+        speedMenu.add(slowItem);
+        speedMenu.add(normalItem);
+        speedMenu.add(fastItem);
+
+        slowItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                SPEED = 200;
+            }
+        });
+
+        normalItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                SPEED = 100;
+            }
+        });
+
+        fastItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                SPEED = 50;
+            }
+        });
+
+        menuBar.add(speedMenu);
     }
 
     private static void configLabels(int[][] array) {
@@ -71,8 +116,12 @@ public class GameLifeRunner {
         for (int i = 0; i < ROW_CELL; i++) {
             for (int j = 0; j < COL_CELL; j++) {
                 int num = array[i][j];
-                String c = num == GameLife.ALIVE_STATE ? "*" : " ";
-                labels[i][j].setText(c);
+                labels[i][j].setText(" ");
+                if (num == 1) {
+                    labels[i][j].setBackground(Color.BLACK);
+                } else {
+                    labels[i][j].setBackground(Color.WHITE);
+                }
             }
         }
 
