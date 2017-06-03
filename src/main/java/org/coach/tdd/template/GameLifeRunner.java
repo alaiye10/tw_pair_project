6f660e4ddd13eb1b58cc691d7f8095c31e023ff8
XvWenJun <906626481@qq.com>
2017-06-03 15:50:24 +0800

    UI binding data

--- src/main/java/org/coach/tdd/template/GameLifeRunner.java
+++ src/main/java/org/coach/tdd/template/GameLifeRunner.java
@@ -1,12 +1,39 @@
 package org.coach.tdd.template;
 
 
-import java.io.IOException;
+import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by XvWenJun on 2017/6/3.
  */
 public class GameLifeRunner {
+
+    private static JLabel[][] labels = new JLabel[10][10];
+    private static JFrame frame;
+
+    private static void createAndShowGUI() {
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        frame = new JFrame("HelloWorldSwing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setSize(200, 200);
+        frame.setLayout(new GridLayout(10, 10));
+
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                JLabel label = new JLabel();
+                label.setText(String.valueOf(i));
+                frame.getContentPane().add(label);
+                labels[i][j] = label;
+            }
+        }
+
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
     public static void main(String[] args) {
         int row = 10;
         int col = 10;
@@ -18,35 +45,56 @@ public class GameLifeRunner {
             }
         }
 
-        printArray(initCellMap);
-
         GameLife gameLife = new GameLife(initCellMap);
-        for (int i = 0; i < 5; i++) {
-            printArray(gameLife.updateCellMap());
+
+        createAndShowGUI();
+        for (int i = 0; i < 50; i++) {
+            int[][] arr=gameLife.updateCellMap();
+
+            configLabels(arr);
+            printArr(arr);
         }
 
     }
 
-    private static void printArray(int[][] array) {
-        clearConsole();
+    private static void printArr(int[][] arr) {
+        for (int j = 0; j <10 ; j++) {
+            for (int k = 0;k < 10; k++)
+                System.out.print(arr[j][k]+"\t");
+            System.out.println();
+        }
+        System.out.println("-----------");
+    }
+
+    private static void configLabels(int[][] array) {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                labels[i][j].setText(String.valueOf(array[i][j]));
+            }
+        }
+
+        frame.repaint();
+
+    }
+
+    private static String descCellMap(int[][] array) {
         int row = array.length;
         int col = array[0].length;
 
+        StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
-                System.out.print(array[i][j] + " ");
+                stringBuilder.append(array[i][j] + " ");
             }
-            System.out.println();
+            stringBuilder.append("\r\n");
         }
+
+        return stringBuilder.toString();
     }
-
-    private static void clearConsole() {
-        try {
-            Runtime.getRuntime().exec("cls");
-        } catch (IOException e) {
-           // e.printStackTrace();
-        }
-    }
-
-
 }
